4b717f368acb25391eed67816562ff160733d932
OrloKun <orlocuario@gmail.com>
2021-01-22 14:00:04 -0300

    hum?

---
+++ RWA_01ServiceApp/Assets/Official Unity Course Assets/Insurance App/assets/Scripts/AWSManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Amazon;
+
+public class AWSManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        UnityInitializer.AttachToGameObject(this.gameObject);    
+    }
+}
