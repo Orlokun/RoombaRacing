09baf72c5b0a229f75a823a6ba38019989090302
orlo <oguerrerofarias@gmail.com>
2021-03-20 20:17:23 -0300

    engine sound added. Should be redone I think

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -23,33 +23,47 @@ public enum SteeringStyle
 public class Driver : MonoBehaviour
 {
     private List<WheelCollider> _wColliders;
-    private List<GameObject> _wObjects;
+    private List<GameObject> _wMeshObjects;
     private Dictionary<WheelCollider, GameObject> _wColMeshDictionary;
     private Transform[] skidTransforms;
+    private List<Light> _brakeLights;
 
     public SteeringStyle actualSteerStyle;
     public AccelerationStyle actualAccelStyle;
 
-    [SerializeField] GameObject wheelCollidersParent;
-    [SerializeField] GameObject wMeshesParentObject;
-    [SerializeField] Transform wheelPrefabMark;
-
+    [SerializeField] private GameObject wheelCollidersParent;
+    [SerializeField] private GameObject wMeshesParentObject;
+    [SerializeField] private Transform wheelPrefabMark;
+    [SerializeField] private GameObject carBody;
+    [SerializeField] private AudioSource highAccel; 
+    //[SerializeField] ParticleSystem wheelSmokePrefab;
+    //private ParticleSystem[] smokeParticles;
     [SerializeField] bool hasVisibleWheels;
-
+    
+    //AccelElements
+    [SerializeField] Rigidbody rBody;
+    [SerializeField] float gearLength = 3;
+    float currentSpeed {get{return rBody.velocity.magnitude * gearLength;}}
+    public float lowPitch = 1f;
+    public float highPitch = 6f;
+    public int numberOfGears = 5;
+    private float rpm;
+    private int currentGear = 1;
+    private float currentGearPerc;
+    public float maxSpeed = 200;
+    
 
     //Steering//
     float maxSteerAngle = 30f;
-
     //Accelerating
     float torqueForce = 1000f;
-
     //Breaking
-    float breakTorque = 5000f;
+    float breakTorque = 10000f;
 
     #region AwakeFunctions
-
     private void Awake()
     {
+        SafetyCheck();
         Initializers();
         GetWheelColliders();
         if (hasVisibleWheels)
@@ -57,15 +71,22 @@ public class Driver : MonoBehaviour
             GetWheelMeshes();
             GenerateWheelDictionary();
         }
+        GetBreakLights();
     }
-
+    void SafetyCheck()
+    {
+        if (wheelCollidersParent == null || wMeshesParentObject == null || wheelPrefabMark == null || carBody == null)
+        {
+            Debug.LogError("ONE OF YOUR SET IN EDITOR OBJECTS IS NOT PLACED");
+        }
+    }
     void Initializers()
     {
         _wColliders = new List<WheelCollider>();
-        _wObjects = new List<GameObject>();
+        _wMeshObjects = new List<GameObject>();
         _wColMeshDictionary = hasVisibleWheels ? new Dictionary<WheelCollider, GameObject>() : null;
+        _brakeLights = new List<Light>();
     }
-
     void GetWheelColliders()
     {
         Transform t = wheelCollidersParent.transform;
@@ -75,22 +96,31 @@ public class Driver : MonoBehaviour
         }
         skidTransforms = new Transform[_wColliders.Count];
     }
-
     void GetWheelMeshes()
     {
         Transform t = wMeshesParentObject.transform;
         for (int i = 0; i < t.childCount; i++)
         {
-            _wObjects.Add(t.GetChild(i).gameObject);
+            _wMeshObjects.Add(t.GetChild(i).gameObject);
         }
     }
-
     void GenerateWheelDictionary()
     {
         if (!hasVisibleWheels) return;
         for (int i = 0; i < _wColliders.Count; i++)
         {
-            _wColMeshDictionary.Add(_wColliders[i], _wObjects[i]);
+            _wColMeshDictionary.Add(_wColliders[i], _wMeshObjects[i]);
+        }
+    }
+    void GetBreakLights()
+    {
+        for (int i = 0; i < carBody.transform.childCount; i++)
+        {
+            if (carBody.transform.GetChild(i).GetComponent<Light>())
+            {
+                Light _light = carBody.transform.GetChild(i).GetComponent<Light>();
+                _brakeLights.Add((_light));
+            }
         }
     }
 
@@ -104,6 +134,7 @@ public class Driver : MonoBehaviour
         float steering = Input.GetAxis("Horizontal");
         float breaking = Input.GetAxis("Jump");
         Go(acceleration, steering, breaking);
+        CalculateEngineSound();
     }
 
     void Go(float accel, float steering, float breakForce)
@@ -158,7 +189,11 @@ public class Driver : MonoBehaviour
 
     void AddForceToWheel(AccelerationStyle accelStyle, WheelCollider wCol, float accel, int wheelId)
     {
-        var thrustTorque = accel * torqueForce;
+        float thrustTorque = 0;
+        if (currentSpeed < maxSpeed)
+        {
+            thrustTorque = accel * torqueForce;
+        }
         switch (accelStyle)
         {
             case AccelerationStyle.FourWheel:
@@ -171,7 +206,6 @@ public class Driver : MonoBehaviour
                     accel = Mathf.Clamp(accel, -1, 1);
                     wCol.motorTorque = thrustTorque;
                 }
-
                 break;
             case AccelerationStyle.RearWheel:
                 if (wheelId > 1)
@@ -206,9 +240,23 @@ public class Driver : MonoBehaviour
         {
             breakForce = Mathf.Clamp(breakForce, -1, 1) * breakTorque;
             wCol.brakeTorque = breakForce;
+            if (breakForce != 0)
+                SetBrakeLightsIntensity(1.8f);
+            else
+            {
+                SetBrakeLightsIntensity(1f);
+            }
         }
     }
 
+    void SetBrakeLightsIntensity(float _intensity)
+    {
+        if (_brakeLights[0].intensity == _intensity) return;
+        foreach (Light _light in _brakeLights)
+        {
+            _light.intensity = _intensity;
+        }
+    }
     void CheckSkid(WheelCollider wCol, int wheelId)
     {
         int skidNumber = 0;
@@ -247,10 +295,38 @@ public class Driver : MonoBehaviour
         holder.rotation = Quaternion.Euler(90, 0, 0);
         Destroy(holder.gameObject, 30f);
     }
+    void CalculateEngineSound()
+    {
+        float gearPerc = (1 / (float)numberOfGears);
+        float targetGearFactor = Mathf.InverseLerp(gearPerc * currentGear, gearPerc * currentGear + 1,
+            Mathf.Abs(currentSpeed / maxSpeed));
+        currentGearPerc = Mathf.Lerp(currentGearPerc, targetGearFactor, Time.deltaTime * 5f);
 
+        var gearNumFactor = (currentGear / (float)numberOfGears);
+        rpm = Mathf.Lerp(gearNumFactor, 1, currentGearPerc);
+
+        float speedPerc = Mathf.Abs(currentSpeed / maxSpeed);
+        float upperGearMax = (1 / (float) numberOfGears) * (currentGear + 1);
+        float downGearMax = (1 / (float) numberOfGears) * (currentGear);
+
+        if (currentGear > 0 && speedPerc < downGearMax)
+        {
+            currentGear--;
+        }
+
+        if (speedPerc > upperGearMax && currentGear < (numberOfGears - 1))
+        {
+            currentGear++;
+        }
+
+        float pitch = Mathf.Lerp(lowPitch, highPitch, rpm);
+        highAccel.pitch = Mathf.Min(highPitch, pitch) * 0.25f;
+    }
 
     #endregion
 
+    
+    
     public void SetState(SteeringStyle _style)
     {
         actualSteerStyle = _style;
